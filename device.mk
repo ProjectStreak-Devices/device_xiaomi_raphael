a57fe65eaf1030673b3b626f32e893330ac44f04
Yung Ti Su <andysu@google.com>
2020-04-27 14:45:07 +0800

    raphael: Enabling sound trigger V2_3 for S5
    Bug: 139071862
    Test: manual
    
    Signed-off-by: Yung Ti Su <andysu@google.com>
    Change-Id: I4123b0af64fc7bed934026540136e3811fab32f3

--- device.mk
+++ device.mk
@@ -56,7 +56,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.bluetooth.audio@2.0-impl \
-    android.hardware.soundtrigger@2.1-impl
+    android.hardware.soundtrigger@2.3-impl
 
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
