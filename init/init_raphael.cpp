d3933a7c64a5dd0116ef1dfe59918dc5fcb3ac30
luca020400 <luca.stefani.ge1@gmail.com>
2020-07-11 10:54:20 +0300

    raphael: enable updating of APEXes
    Change-Id: Ic1469103425e5345f0db88e40e93fd600907e5c9
    Signed-off-by: Raaj52 <mail2raaj52@gmail.com>

--- init/init_raphael.cpp
+++ init/init_raphael.cpp
@@ -104,6 +104,7 @@ void vendor_load_properties() {
     property_override_multifp("ro.build.fingerprint", "ro.system.build.fingerprint", "ro.bootimage.build.fingerprint",
 	    "ro.vendor.build.fingerprint", "google/coral/coral:11/RP1A.201005.004/6782484:user/release-keys");
     property_override("ro.control_privapp_permissions", "log");
+    property_override("ro.apex.updatable", "true");
 
     load_dalvikvm_properties();
 }
