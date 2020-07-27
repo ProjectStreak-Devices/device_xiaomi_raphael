cbc805f9bc229cdda264535c57e14cd25151a0f4
Raaj52 <mail2raaj52@gmail.com>
2020-07-27 13:16:55 +0530

    raphael: prebuilts: Add GCam GO
    Signed-off-by: Raaj52 <mail2raaj52@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -287,6 +287,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_msmnile
 
+# Prebuilt Packages
+PRODUCT_PACKAGES += \
+    GCamGo
+
 # Media
 PRODUCT_PACKAGES += \
     libarbitrarybytes \
