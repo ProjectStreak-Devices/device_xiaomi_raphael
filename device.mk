f6787c5d476861ec0e34d4e067e3ad2b2cc4b536
Lucchetto <lucchetto.tie@live.com>
2020-09-19 12:02:29 +0530

    raphael: build libavservices_minijail_vendor
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- device.mk
+++ device.mk
@@ -142,7 +142,8 @@ PRODUCT_COPY_FILES += \
 # Codec2
 PRODUCT_PACKAGES += \
     libcodec2_vndk.vendor \
-    libcodec2_hidl@1.0.vendor
+    libcodec2_hidl@1.0.vendor \
+    libavservices_minijail_vendor
 
 # Context hub HAL
 PRODUCT_PACKAGES += \
