6e3bfe52a7e7d9c4c43fd35f6445a62146f5a292
Raaj52 <mail2raaj52@gmail.com>
2020-10-07 18:11:33 +0000

    Revert "[DNM]raphael: don't use telephony jar"
    This reverts commit 6d492a3be2ee7ad947af79c67c14de34a4ed4db0.

--- device.mk
+++ device.mk
@@ -173,8 +173,8 @@ PRODUCT_PACKAGES += \
     qti_telephony_utils.xml \
     telephony-ext
 
-#PRODUCT_BOOT_JARS += \
-#    telephony-ext
+PRODUCT_BOOT_JARS += \
+    telephony-ext
 
 # TextClassifier
 PRODUCT_PACKAGES += \
