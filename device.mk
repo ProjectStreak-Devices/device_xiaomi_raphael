93425c51c4f5b14cb48472c584ffd60c83d9940f
HSgod <hsgodkontakt@gmail.com>
2020-10-09 10:54:02 +0000

    raphael: Build libgui_vendor from source
    Signed-off-by: HSgod <hsgodkontakt@gmail.com>

--- device.mk
+++ device.mk
@@ -140,7 +140,8 @@ PRODUCT_COPY_FILES += \
 # Camera
 PRODUCT_PACKAGES += \
     libdng_sdk.vendor \
-    libqti-perfd-client
+    libqti-perfd-client \
+    libgui_vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
