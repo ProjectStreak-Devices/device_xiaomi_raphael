402c1fe854dc816090688e61fde72498e7a27369
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-10-11 10:14:12 +0000

    raphael: Remove BOARD_SEPOLICY_M4DEFS
    * Sepolicy is already updated as per QCOM changes
    
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -186,11 +186,6 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-BOARD_SEPOLICY_M4DEFS += \
-    wcnss_service_exec=vendor_wcnss_service_exec \
-    sysfs_ssr=vendor_sysfs_ssr \
-    public_vendor_default_prop=vendor_public_vendor_default_prop \
-    sensors_prop=vendor_sensors_prop
 
 # Vendor init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_raphael
