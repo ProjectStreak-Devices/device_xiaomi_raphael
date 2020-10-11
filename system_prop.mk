6f117b4fec594df087c571f39fc14360fcd44ae9
Raaj52 <mail2raaj52@gmail.com>
2020-10-11 05:56:27 +0000

    raphael: props: Drop soft_backlight
    Signed-off-by: Raaj52 <mail2raaj52@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -45,7 +45,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.disable_backpressure=1 \
     debug.sf.latch_unsignaled=1 \
     debug.sf.enable_hwc_vds=1
 
