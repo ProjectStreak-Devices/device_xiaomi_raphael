ac52c29c1287b30ed5c6c088a52013bb8b033ec4
HSgod <hsgodkontakt@gmail.com>
2020-10-12 18:29:07 +0000

    Revert "raphael: Disable soundtrigger"
    This reverts commit 8654d379cecc2793a7039d12f502f58b56dd2e43.

--- device.mk
+++ device.mk
@@ -64,7 +64,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
-    android.hardware.bluetooth.audio@2.0-impl
+    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.soundtrigger@2.1-impl
 
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
