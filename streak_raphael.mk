eff48b9d7d026de4b4149b3a1315fbafdbbdbac9
Alexandru Scurtu <sasha.scurtu@gmail.com>
2020-10-30 14:10:47 +0200

    raphael: common -> common_full_phone
    Signed-off-by: Alexandru Scurtu <sasha.scurtu@gmail.com>

--- streak_raphael.mk
+++ streak_raphael.mk
@@ -16,7 +16,7 @@
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
 # Inherit some common RevengeOS stuff.
-$(call inherit-product, vendor/streak/config/common.mk)
+$(call inherit-product, vendor/streak/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
