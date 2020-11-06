de92e2dc43e8fe8106bac3a0476c15f172ce04ac
HSgod <hsgodkontakt@gmail.com>
2020-11-06 11:27:51 +0000

    raphael: Streak-ify
    Signed-off-by: HSgod <hsgodkontakt@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -13,9 +13,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/yaap_raphael.mk
+    $(LOCAL_DIR)/streak_raphael.mk
 
 COMMON_LUNCH_CHOICES := \
-    yaap_raphael-user \
-    yaap_raphael-userdebug \
-    yaap_raphael-eng
+    streak_raphael-user \
+    streak_raphael-userdebug \
+    streak_raphael-eng
