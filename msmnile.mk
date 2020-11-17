6b5f63eecee578128d19aa2ce37d0295890605aa
Luca Stefani <luca.stefani.ge1@gmail.com>
2020-11-17 22:35:32 +0100

    raphael: Switch to AOSP NFC
    Change-Id: I9f62b369181c75e8ad5e0b8504f6c2fbb292f894

--- msmnile.mk
+++ msmnile.mk
@@ -347,14 +347,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
 # NFC
-PRODUCT_SOONG_NAMESPACES += \
-    vendor/nxp/opensource/sn100x
-
-# NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service \
     NfcNci \
     Tag \
+    SecureElement \
     com.android.nfc_extras
 
 PRODUCT_COPY_FILES += \
