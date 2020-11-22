64668000924d76dc69c45e22d38025df5aac49b1
HSgod <hsgodkontakt@gmail.com>
2020-11-22 17:20:11 +0100

    raphael: Build gapps and apex
    Signed-off-by: HSgod <hsgodkontakt@gmail.com>

--- streak_raphael.mk
+++ streak_raphael.mk
@@ -20,6 +20,8 @@ $(call inherit-product, vendor/streak/config/common_full_phone.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
+TARGET_BUILD_GAPPS := true
+TARGET_SUPPORTS_UPDATABLE_APEX := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := streak_raphael
